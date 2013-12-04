5d80ea280d3e1a496e6e0f90a787f6294a81f140
Fabio Rehm <fgrehm@gmail.com>
2013-12-04 00:13:19 -0200

    First stab at getting the server to work locally

--- viewdocs.go
+++ viewdocs.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"strings"
 	"time"
 
@@ -26,18 +25,8 @@ func (cv *CacheValue) Size() int {
 	return len(cv.Value)
 }
 
-func parseRequest(r *http.Request) (user, repo, ref, doc string, err error) {
-	hostname := strings.Split(r.Host, ".")
-	user = hostname[0]
+func parseRequest(r *http.Request) (doc string, err error) {
 	path := strings.Split(r.RequestURI, "/")
-
-	repoAndRef := strings.Split(path[1], "~")
-	repo = repoAndRef[0]
-	if len(repoAndRef) == 1 {
-		ref = "master"
-	} else {
-		ref = repoAndRef[1]
-	}
 
 	if len(path) < 3 || (len(path) == 3 && strings.HasSuffix(r.RequestURI, "/")) {
 		doc = "index"
@@ -50,59 +39,33 @@ func parseRequest(r *http.Request) (user, repo, ref, doc string, err error) {
 	return
 }
 
-func fetchAndRenderDoc(user, repo, ref, doc string) (string, error) {
+func fetchAndRenderDoc(doc string) (string, error) {
 	template := make(chan string)
 	go func() {
-		resp, err := http.Get("https://raw.github.com/" + user + "/" + repo + "/" + ref + "/docs/template.html")
-		if err != nil || resp.StatusCode == 404 {
-			template <- DefaultTemplate
-			return
-		}
-		body, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		buf, err := ioutil.ReadFile("docs/template.html")
 		if err != nil {
 			template <- DefaultTemplate
 			return
 		}
-		template <- string(body)
+		template <- string(buf)
 	}()
-	resp, err := http.Get("https://raw.github.com/" + user + "/" + repo + "/" + ref + "/docs/" + doc + ".md")
+	buf, err := ioutil.ReadFile("docs/" + doc + ".md")
+	if err != nil {
+		return "# Page not found", err
+	}
+
+	bodyStr := string(buf)
+	resp, err := http.Post("https://api.github.com/markdown/raw?access_token="+os.Getenv("ACCESS_TOKEN"), "text/x-markdown", strings.NewReader(bodyStr))
 	if err != nil {
 		return "", err
 	}
-	var body []byte
-	if resp.StatusCode == 200 {
-		body, err = ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			return "", err
-		}
-	} else {
-		resp.Body.Close()
-		body = []byte("# Page not found")
-	}
-
-	bodyStr := string(body)
-	// Ajust relative links for specific git revisions
-	// FIXME: This doesn't handle relative links on the template / layout,
-	//        just on the markdown page itself
-	if ref != "master" {
-		reg := regexp.MustCompile(`(\[[^\]]+\]\()(/` + repo + `)([^~)])`)
-		bodyStr = reg.ReplaceAllString(bodyStr, "$1$2~"+ref+"$3")
-	}
-
-	resp, err = http.Post("https://api.github.com/markdown/raw?access_token="+os.Getenv("ACCESS_TOKEN"), "text/x-markdown", strings.NewReader(bodyStr))
-	if err != nil {
-		return "", err
-	}
-	body, err = ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
 	output := strings.Replace(<-template, "{{CONTENT}}", string(body), 1)
-	output = strings.Replace(output, "{{NAME}}", repo, -1)
-	output = strings.Replace(output, "{{USER}}", user, -1)
+	// output = strings.Replace(output, "{{NAME}}", repo, -1)
+	// output = strings.Replace(output, "{{USER}}", user, -1)
 	return output, nil
 }
 
@@ -131,27 +94,23 @@ func main() {
 	DefaultTemplate = string(body)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.RequestURI == "/" {
-			http.Redirect(w, r, "http://progrium.viewdocs.io/viewdocs", 301)
-			return
-		}
 		if r.RequestURI == "/favicon.ico" {
 			return
 		}
 		switch r.Method {
 		case "GET":
-			user, repo, ref, doc, err := parseRequest(r)
+			doc, err := parseRequest(r)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				w.Write([]byte(err.Error()))
 				return
 			}
-			log.Printf("Building docs for '%s/%s' (ref: %s)", user, repo, ref)
-			key := user + ":" + repo + ":" + doc + ":" + ref
+			log.Printf("Building docs for '%s'", doc)
+			key := doc
 			value, ok := lru.Get(key)
 			var output string
 			if !ok {
-				output, err = fetchAndRenderDoc(user, repo, ref, doc)
+				output, err = fetchAndRenderDoc(doc)
 				if err != nil {
 					w.WriteHeader(http.StatusInternalServerError)
 					w.Write([]byte(err.Error()))
